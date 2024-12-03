fe8f935c1e3d53d6dceeaba85190837d84c87c94
0xvikasrushi <0xvikas@gmail.com>
2024-12-03 23:05:25 +0530

    fix: change .dat loc

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const fileUrls = {
   'aadhaar-verifier.dat':
-    'https://anon-aadhaar-artifacts.s3.eu-central-1.amazonaws.com/v2.0.0/aadhaar-verifier.dat',
+    'https://raw.githubusercontent.com/0xVikasRushi/witnesscalc/refs/heads/main/src/aadhaar_verifier.dat',
   'vkey.json':
     'https://anon-aadhaar-artifacts.s3.eu-central-1.amazonaws.com/v2.0.0/vkey.json',
   'circuit_final.zkey':
